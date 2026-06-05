70a5a39644b15d5b570c67d799509ed5f64ce7a7
arturo179 <artio9000@gmail.com>
2026-06-04 17:17:58 -0700

    update to handle images

--- el_parrillaso/frontend/src/pages/reviews.jsx
+++ el_parrillaso/frontend/src/pages/reviews.jsx
@@ -1,10 +1,10 @@
-import { useEffect,useState } from "react";
+import { useEffect, useState } from "react";
 import { supabase } from "../services/supabaseClient"
 
 import { Navigate, NavLink, useNavigate } from "react-router-dom";
 
 
-function handle_reciew(){
+function handle_reciew() {
     Navigate("/reviews");
 
 
@@ -15,21 +15,21 @@ function Reviews() {
     const [reviewText, setReviewText] = useState("");
     const [rating, setRating] = useState(0);
     const [image, setImage] = useState(null);
-    const[preview, setPreview] = useState(null);
+    const [preview, setPreview] = useState(null);
     const [reviews, setReviews] = useState([]);
-    const[imageUrl, setImageUrl] = useState("");
+    const [imageUrl, setImageUrl] = useState("");
     const [formError, setFormError] = useState("");
-    
+
 
     useEffect(() => {
-        const fetchReviews = async() => {
+        const fetchReviews = async () => {
             try {
                 const API_URL = import.meta.env.VITE_API_URL;
                 const res = await fetch(`${API_URL}/reviews`);
                 const data = await res.json();
                 setReviews(data);
 
-            } catch(err) {
+            } catch (err) {
                 console.error("load error:", err.message);
             }
         };
@@ -43,62 +43,62 @@ function Reviews() {
     const handleSubmit = async (e) => {
         e.preventDefault();
         setFormError("");
-        const {data: { session} } = await supabase.auth.getSession();
+        const { data: { session } } = await supabase.auth.getSession();
 
 
-        if (!session) {
-            setFormError("You must log in or create an account to submit a review.");
-            return;
-        }
 
-        if(!reviewText.trim() && rating === 0){
+
+        if (!reviewText.trim() && rating === 0) {
             setFormError("Please add a rating and write a reivew before Submitting")
             return;
         }
-        if(!rating==0){
+        if (rating == 0) {
             setFormError("Please have all fienlds filled")
             return;
         }
-        if(!reviewText.trim()){
+        if (!reviewText.trim()) {
             setFormError("Please write something before Submitting");
             return;
         }
-         const API_URL = import.meta.env.VITE_API_URL;
+
+
+        const API_URL = import.meta.env.VITE_API_URL;
 
         try {
 
-            
-
-        
+            if (!session) {
+                setFormError("You must log in or create an account to submit a review.");
+                return;
+            }
 
             let uploadedUrl = null;
-            if(image){
+            if (image) {
                 uploadedUrl = await uploadImage();
             }
 
             const formData = new FormData();
-            formData.append("comment",reviewText);
-            formData.append("rating",rating);
+            formData.append("comment", reviewText);
+            formData.append("rating", rating);
             if (image) {
-                formData.append("image",image);
+                formData.append("image", image);
                 console.log(formData.get("image"));
             }
 
             else {
                 console.log("no image");
             }
-            
-            if(uploadedUrl) formData.append("image_url", uploadedUrl);
+
+            if (uploadedUrl) formData.append("image_url", uploadedUrl);
             const res = await fetch(`${API_URL}/reviews`, {
                 method: "POST",
-                headers:{
+                headers: {
                     Authorization: `Bearer ${session.access_token}`,
                 },
                 body: formData,
             });
             const data = await res.json();
 
-            if(!res.ok){
+            if (!res.ok) {
                 throw new Error(data.error || "Failed to submit review");
             }
 
@@ -107,46 +107,52 @@ function Reviews() {
             setRating(0);
             setImage(null);
             setPreview(null);
-            } catch(err){
-                console.error("submit error:", err.message)
-            }
-            
+        } catch (err) {
+            console.error("submit error:", err.message)
+        }
+
     };
     const HandleImageChange = (e) => {
         const file = e.target.files[0];
+        const allowedTypes = ["image/jpeg", "image/png", "image/jpg"];
+        if (!allowedTypes.includes(file.type)) {
+            setFormError("Only JPG and PNG images are allowed.");
+            return;
+        }
         if (file) {
             setImage(file);
             setPreview(URL.createObjectURL(file))
         }
 
+
     };
 
     const uploadImage = async () => {
-    const fileName = `${Date.now()}-${image.name}`;
-    try {
-        const { data: imageData, error: uploadError } = await supabase.storage
-            .from("uploads")
-            .upload(fileName, image);
-
-        if (uploadError) {
-            console.log("Supabase error", uploadError);
-            return null;
-        }
-
-        if (imageData) {
-            const { data: publicUrlData } = supabase.storage
+        const fileName = `${Date.now()}-${image.name}`;
+        try {
+            const { data: imageData, error: uploadError } = await supabase.storage
                 .from("uploads")
-                .getPublicUrl(fileName);  
-            if (publicUrlData?.publicUrl) {
-                setImageUrl(publicUrlData.publicUrl);
-                return publicUrlData.publicUrl;
+                .upload(fileName, image);
+
+            if (uploadError) {
+                console.log("Supabase error", uploadError);
+                return null;
             }
+
+            if (imageData) {
+                const { data: publicUrlData } = supabase.storage
+                    .from("uploads")
+                    .getPublicUrl(fileName);
+                if (publicUrlData?.publicUrl) {
+                    setImageUrl(publicUrlData.publicUrl);
+                    return publicUrlData.publicUrl;
+                }
+            }
+        } catch (error) {
+            console.log("caught an error", error);
         }
-    } catch (error) {
-        console.log("caught an error", error);
-    }
-    return null;
-};
+        return null;
+    };
 
 
 
@@ -195,34 +201,35 @@ function Reviews() {
         </form>
 
         <div className="review-list">
-            {reviews.map((r, index) => { 
+            {reviews.map((r, index) => {
                 console.log("review data:", r.image_url);
                 return (
-                <div key={r.id} className="review-card">
+                    <div key={r.id} className="review-card">
 
-                    <div className="review-header">
-                        <div className="review-rating">
-                            {"⭐".repeat(r.rating)}
+                        <div className="review-header">
+                            <div className="review-rating">
+                                {"⭐".repeat(r.rating)}
+                            </div>
+                            <span className="review-date">Today</span>
                         </div>
-                        <span className="review-date">Today</span>
+
+                        <p className="review-text">{r.comment}</p>
+
+                        {(r.image_url || r.image_URL) && (
+                            <img
+                                src={r.image_url || r.image_URL}
+                                alt="Review"
+                                className="review-image"
+                            />
+
+
+                        )}
+
+
                     </div>
 
-                    <p className="review-text">{r.comment}</p>
-
-                    {(r.image_url || r.image_URL) && (
-                        <img
-                            src= {r.image_url || r.image_URL}
-                            alt="Review"
-                            className="review-image"
-                        />
-                            
-
-                    )}
-                    
-
-                </div>
-
-            )})}
+                )
+            })}
         </div>
 
 
